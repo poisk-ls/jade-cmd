693a3cfa31e39150a5d889421d08f0756e112e3d
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-27 22:52:43 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install ruby -y
+pkg install zip -y
+pkg install unzip -y
+pkg install curl -y
+pkg install cmatrix -y
+pkg install fish -y
+pkg install figlet -y
+pkg install neofetch -y
+pkg install bash -y
+pkg install clang -y
+pkg install wget -y
+pkg install termux-api -y
+pkg install chroot -y
+pkg install golang -y
+pkg install sl -y
+pip install requests
+pip2 install requests
+pip3 install requests
+pip install php
+pip2 install php
+pip3 install php
+pip install mechanize
+pip2 install mechanize
+pip3 install mechanize
