103950260c92e70f7f3b379d77f11751b61f0d9d
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-28 10:23:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,74 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bash
 
-pkg install git -y
-pkg install python -y
+echo $red
+
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " this packages made by •poisk-ls• "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
+pkg install git -y                            #update in progress
+pkg install python -y                         #installation......
 pkg install python2 -y
 pkg install python3 -y
 pkg install ruby -y
+
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Follow me on Github🖤🥀 "
+echo " https://github.com/poisk-ls "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
 pkg install zip -y
 pkg install unzip -y
 pkg install curl -y
 pkg install cmatrix -y
+
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " DM me directly🖤🥀 "
+echo " https://m.me/speedy.mmsc80.thugs "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
 pkg install fish -y
 pkg install figlet -y
 pkg install neofetch -y
 pkg install bash -y
 pkg install clang -y
+
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Add me on FB🖤🥀 "
+echo " ☣️Jade Nelson Buhayan☣️ "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
 pkg install wget -y
 pkg install termux-api -y
 pkg install chroot -y
 pkg install golang -y
+
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Follow me on Github🖤🥀 "
+echo " https://github.com/poisk-ls "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
 pkg install sl -y
 pip install requests
 pip2 install requests
 pip3 install requests
+
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Follow me on Telegram🖤🥀 "
+echo " https://github.com/poisk-ls "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
 pip install php
 pip2 install php
 pip3 install php
 pip install mechanize
 pip2 install mechanize
 pip3 install mechanize
+
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Keep safe🥀💛 "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
+echo "☣️Your packages have  installed successfully☣️ "
+
+exit
